dea89927ef00b142be30ae95574f7525f1738d9a
RezHackXYZ <rhythmupa@gmail.com>
2025-04-03 10:36:45 +0530

    m nb

--- src/app.svelte
+++ src/app.svelte
@@ -2,18 +2,25 @@
 	import Router from "svelte-spa-router";
 
 	import Home from "./pages/home.svelte";
-	import a from "./pages/a.svelte";
-	import b from "./pages/b.svelte";
-	import c from "./pages/c.svelte";
+	import config from "./pages/config.svelte";
 	let routes = {
 		"/": Home,
-		"/a": a,
-		"/b": b,
-		"/c": c,
+		"/config": config,
 	};
 
 	import Nav from "./nav.svelte";
 </script>
 
-<Nav />
-<Router {routes} />
+<div id="root">
+	<Nav />
+	<Router {routes} />
+</div>
+
+<style>
+	#root {
+		height: 100%;
+		width: 100%;
+		background-color: black;
+		color: white;
+	}
+</style>
---
+++ src/pages/config.svelte
@@ -0,0 +1,5 @@
+Get a Googel search api key from: https://developers.google.com/custom-search/v1/overview#api_key
+AIzaSyDvtO_p0haF6TrZv8RzfpY8CAg_TGRRUiw
+
+And a gemini api key from: https://aistudio.google.com/apikey
+AIzaSyCoC01uwBqrBIvK3Vo9xWLGCLTRkLPrFd0
